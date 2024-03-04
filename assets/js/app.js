0e88c8cc6c6aa14657f9ad572f530d5056e4a738
Igor Boskovic <139734677+boskovicIgor@users.noreply.github.com>
2024-03-04 16:10:20 +0100

    Search toggle for mobile device

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
+"use strict";
 
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/* Add event listener on multiple elements */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+/* Toggle search in mobile devices */
+const searchView = document.querySelector(".search-view");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => {
+  searchView.classList.add("active");
+};
+
+addEventOnElements(searchTogglers, "click", toggleSearch);
